cfc5258e980b581550737b075b3f153123c98a6b
amin91s <30566174+amin91s@users.noreply.github.com>
2022-11-26 03:20:01 +0100

    folder for ttp stuff

---
+++ ttp.c
@@ -0,0 +1,5 @@
+//
+// Created by Amin Soleimani on 26/11/2022.
+//
+#include "ttp.h"
+
---
+++ ttp.h
@@ -0,0 +1,8 @@
+//
+// Created by Amin Soleimani on 26/11/2022.
+//
+
+#ifndef FRAMEWORK_TTP_H
+#define FRAMEWORK_TTP_H
+
+#endif //FRAMEWORK_TTP_H
